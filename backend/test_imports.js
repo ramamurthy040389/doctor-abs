391bf5d7f077cd22020bcfdfb1aedf402272fbf2
ramamurthy040389 <ramu5454@gmail.com>
2025-12-04 17:14:45 +0530

    chore: Delete file.

--- backend/test_imports.js
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-
-console.log('Express:', typeof express);
-console.log('Mongoose:', typeof mongoose);
-console.log('Dotenv:', typeof dotenv);

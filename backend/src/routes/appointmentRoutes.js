6c3b414cadaa3925ec07f55a2eea80f016eb440b
ramamurthy040389 <31787949+ramamurthy040389@users.noreply.github.com>
2025-12-04 17:30:13 +0530

    Delete backend directory

--- backend/src/routes/appointmentRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { protect } = require('../middlewares/auth');
-const appointmentController = require('../controllers/appointmentController');
-
-router.post('/book', protect, appointmentController.book);
-
-module.exports = router;
--- backend/src/routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-// GET /api/users
-router.get('/', (req, res) => {
-  res.json({ message: 'users list (placeholder)' });
-});
-
-module.exports = router;
